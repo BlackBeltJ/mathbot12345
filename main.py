01d519db4308fb76f34bd9a601bfed6deef31150
Joshua Edwards <BlackBeltJJE@gmail.com>
2022-01-19 18:15:48 -0700

    Made a lot of changes regarding pips, dotenv, tokens, and discord. Trying to get Vscode to recognize installed modules now

--- main.py
+++ main.py
@@ -1,58 +1,55 @@
-from posix import environ
-import discord
-import time
 import os
-import requests
-import json
-import random
-import math 
+# import discord
+# import time
+# import requests
+# import random
+# import math 
 
 
-TOKEN = os.getenv("TOKEN")
+token = os.getenv("TOKEN")
+print("token is " + token)
 
-# token = TOKEN
-
-client = discord.Client()
-
-
-# from token import TOKEN
-
-# def read_token():
- # with open("token.env", "r") as f:
- #   lines = f.readlines()
- #   return lines[0].strip()
+print("Hello")
 
 
 
+###### client = discord.Client()
+
+#def read_token():
+#   with open(".env", "r") as f:
+#      lines = f.readlines()
+#      return lines[0].strip()
+
+# token = read_token()
 
 
-def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]["q"] + " -" + json_data[0]["a"]
-  return(quote)
+# def get_quote():
+ #  response = requests.get("https://zenquotes.io/api/random")
+ #  json_data = json.loads(response.text)
+ #  quote = json_data[0]["q"] + " -" + json_data[0]["a"]
+ #  return(quote)
 
 
-@client.event
-async def on_ready():
-  print("We have logged in as {0.user}".format(client))
+# @client.event
+ #async def on_ready():
+ #  print("We have logged in as {0.user}".format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# @client.event
+# async def on_message(message):
+   # if message.author == client.user:
+    #     return
 
-    msg = message.content
+   #  msg = message.content
 
-    if msg.startswith("$calc"):
-      await message.channel.send('calcualting...')
+   #  if msg.startswith("$calc"):
+    #   await message.channel.send('calcualting...')
 
-    if msg.startswith('$hello'):
-        await message.channel.send('Hello!')
+    # if msg.startswith('$hello'):
+    #     await message.channel.send('Hello!')
 
-    if msg.startswith('$inspire'):
-      quote = get_quote()
-      await message.channel.send(quote)
+   #  if msg.startswith('$inspire'):
+    #   quote = get_quote()
+    #   await message.channel.send(quote)
 
 
-client.run(TOKEN)
+ #client.run(TOKEN)
