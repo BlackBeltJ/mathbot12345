c2464219f01ef83d44d33e58c9c11149bb42e0d3
Joshua Edwards <BlackBeltJJE@gmail.com>
2022-01-31 17:46:24 -0700

    indent in main.py

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ client = discord.Client()
 
 
 @client.event
- async def on_ready():
-     print("We have logged in as {0.user}".format(client))
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
 
 
 client.run(token)
